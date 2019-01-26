e55a24760c0ff853e5603592aaaca40a232c59da
m@tthias.space <m@tthias.space>
2019-01-26 04:38:07 +0100

    add sendValue javascript function

--- py/index.js
+++ py/index.js
@@ -1,6 +1,24 @@
 var exec = require('child_process').exec;
 
-console.log('start sniffer-process . . .');
-exec('./bin/sniffer', (error, stdout, stderr) => {
-    console.log(stdout);
-});
+var ON = '5506385';
+var OFF = '5506388';
+
+var status = false;
+
+function sendValue(val) {
+    exec('./bin/send ' + val, (error, stdout, stderr) => { console.log(stdout);});
+}
+
+while(true) {
+
+    setTimeout(() => {
+        status = !status;
+        sendValue((status ? ON : OFF));
+        sendValue((status ? ON : OFF));
+        sendValue((status ? ON : OFF));
+        sendValue((status ? ON : OFF));
+        sendValue((status ? ON : OFF));
+    }, 2000);
+
+}
+
